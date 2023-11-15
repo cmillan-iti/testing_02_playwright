b17cdb36a607c369c133e6b0dc7449783a156c2a
Carlos Millán Navarro <cmillan@iti.es>
2023-11-15 15:31:26 +0100

    Initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,37 @@
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using System.Text.RegularExpressions;
+
+namespace PlaywrightDemoTests
+{
+
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class Tests : PageTest
+    {
+        [Test]
+        public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+        {
+            await Page.GotoAsync("https://playwright.dev");
+
+            Thread.Sleep(3000);
+
+            // Expect a title "to contain" a substring.
+            await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+
+            // create a locator
+            var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
+
+            // Expect an attribute "to be strictly equal" to the value.
+            await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+
+            // Click the get started link.
+            await getStarted.ClickAsync();
+
+            // Expects the URL to contain intro.
+            await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
+
+            Thread.Sleep(3000);
+        }
+    }
+}
